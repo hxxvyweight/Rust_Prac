4245208668360b54b6802a578d747d1057d371b0
hxxyweight <linuxtest923@gmail.com>
2026-04-02 10:08:14 +1100

    New Cargo Build Created

---
+++ Rust_Prac/target/debug/Rust_Prac.d
@@ -0,0 +1,1 @@
+/home/hxxvyweight/Desktop/Rust/Rust_Prac/Rust_Prac/target/debug/Rust_Prac: /home/hxxvyweight/Desktop/Rust/Rust_Prac/Rust_Prac/src/main.rs
---
+++ Rust_Prac/target/debug/deps/Rust_Prac-226f6b061c96ad89.d
@@ -0,0 +1,5 @@
+/home/hxxvyweight/Desktop/Rust/Rust_Prac/Rust_Prac/target/debug/deps/Rust_Prac-226f6b061c96ad89.d: src/main.rs
+
+/home/hxxvyweight/Desktop/Rust/Rust_Prac/Rust_Prac/target/debug/deps/libRust_Prac-226f6b061c96ad89.rmeta: src/main.rs
+
+src/main.rs:
