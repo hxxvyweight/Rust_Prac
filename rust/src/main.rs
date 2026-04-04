445b6ad1b5881f0b32ebc23709d4c80a3d705220
hxxyweight <linuxtest923@gmail.com>
2026-04-04 15:02:03 +1100

    Stopping Text Game Project, have learnt better ways to do this.  Moving onto another

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,83 @@
 use std::io;
 
+fn return_outcome(success: bool, level: &mut u32, current_xp: &mut u32, required_xp: &mut u32) {
+        
+    let mut encountered: bool = true;
+       
+    while encountered {
+            
+        if success {
+            *current_xp += 25;
+            *required_xp -= 25;
+            println!("Level: {}, Current XP: {}, Required: {}", level, current_xp, required_xp);
+            encountered = false;
+        } else {
+            println!("Try again!");
+        }
+    }
+}
+
+fn encounter() -> bool {
+        
+    let mut enemy_hp: i32 = 250;
+    let enemy_dmg: i32 = 25;
+    let mut user_hp: i32 = 250;
+    let user_dmg: i32 = 50;
+    let mut killcount: u32 = 0;
+
+
+    loop {  
+            
+        let mut user_choice:String = String::new();
+        user_choice.clear();
+       
+        println!("An enemy stands before you!\n");
+
+            //Reading for User Input and assigning to variable//
+            
+        println!("Attack or defend\n");
+           
+
+        io::stdin()
+            .read_line(&mut user_choice)
+            .expect("Failed to read_line");
+            let user_choice = user_choice.trim().to_lowercase();    
+                //Getting Input and Trimming Whitespace// 
+
+                
+                
+            if user_choice == "attack"{
+                enemy_hp -= user_dmg;
+                println!("\nUser HP: {} | Enemy HP: {}\n", user_hp, enemy_hp);
+
+                if enemy_hp <= 0 {
+                    println!("Enemy defeated! {}\n", enemy_hp);
+                    killcount += 1;
+                    println!("Enemies Defeated: {}", killcount);
+                    
+                    return true;
+                }
+                
+            }else if user_choice == "defend" {
+                user_hp -= enemy_dmg;
+                println!("\nUser HP: {} | Enemy HP: {}\n", user_hp, enemy_hp);
+                
+                if user_hp <= 0 {
+                    println!("Game Over...\n");
+                    println!("You Defeated {} Enemies!!!\nCongratulations", killcount); 
+                    return true;
+                }
+            }
+    }
+}
+
+
+fn prompt_user() {
+    println!("Press ENTER to continue");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+}
+
 fn main() {
     
 
@@ -13,11 +91,6 @@ fn main() {
 
     println!("Basic Text Game!\n");
     
-    fn prompt_user() {
-        println!("Press ENTER to continue");
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-    }
    
     loop {
         prompt_user();
@@ -25,7 +98,7 @@ fn main() {
     }
      
     //Basic Level, Stage, and Available Class Names//
-    const MAP: &str = "Map";
+    const MAP: &str = "MAP";
 
     println!("Welcome to {}\n", MAP);
    
@@ -64,76 +137,14 @@ fn main() {
         required_xp: 200,
     };
     
-
-    //Basic Encounter System for Combat//
-    fn return_outcome(success: bool, level: &mut u32, current_xp: &mut u32, required_xp: &mut u32) {
-        
-        let mut encountered: bool = true;
-       
-        while encountered {
-            
-            if success {
-                *current_xp += 25;
-                *required_xp -= 25;
-                println!("Level: {}, Current XP: {}, Required: {}", level, current_xp, required_xp);
-                encountered = false;
-           } else {
-               println!("Try again!");
-            }
-        }
+    let mut action = prompt_user();
+    if action == "fight" {
+        let success = encounter();
     }
-
-
-    fn encounter() -> bool {
-        
-        let mut enemy_hp: i32 = 250;
-        let enemy_dmg: i32 = 25;
-        let mut user_hp: i32 = 500;
-        let user_dmg: i32 = 50;
-
-
-        loop {  
-            
-            let mut user_choice:String = String::new();
-            user_choice.clear();
-       
-            println!("An enemy stands before you!\n");
-
-            //Reading for User Input and assigning to variable//
-            
-            println!("Attack or defend\n");
-           
-
-            io::stdin()
-                .read_line(&mut user_choice)
-                .expect("Failed to read_line");
-                let user_choice = user_choice.trim().to_lowercase();    
-                //Getting Input and Trimming Whitespace// 
-
-                
-                
-                if user_choice == "attack"{
-                    enemy_hp -= user_dmg;
-                    println!("\nUser HP: {} | Enemy HP: {}\n", user_hp, enemy_hp);
-
-                    if enemy_hp <= 0 {
-                        println!("Enemy defeated! {}\n", enemy_hp);
-                        return true;
-                
-                }else if user_choice == "Defend" {
-                    user_hp -= enemy_dmg;
-                    println!("You lost {}\n\nUser HP: {}", enemy_dmg, user_hp);
-                    continue;
-                }
-                }
-        }
-    }
-    
-
-    let success = encounter();
     
     return_outcome(success, &mut xp_pool.level, &mut xp_pool.current_xp, &mut xp_pool.required_xp);
    
     println!("- Strength: {}\n- Magic: {}\n- Stamina: {}\n", stats.strength, stats.magic, stats.stamina);
 
 }
+
